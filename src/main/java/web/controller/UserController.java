0a9fd17d9d58146e2988f163ff78f2d2c648ebd7
Wiack <viiakko@gmail.com>
2023-02-18 17:23:53 +0300

    1

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -27,28 +27,30 @@ public class UserController {
         return "allUser";
     }
 
+    //Методы должны быть глаголами ...  (в смешанном регистре с первой строчной буквой, с первой буквой каждого внутреннего слова с заглавной буквы)
+
     @GetMapping("/newUser")
-    public String newUser(Model model) {
-        model.addAttribute("user", new User());
+    public String createNewUser(Model model) {
+        model.addAttribute("user", new User()); //создаем нового пользователя
         return "detailsNewUser";
     }
 
     @PostMapping()
-    public String createNewUser(@ModelAttribute("user") User user) {
-        userService.saveUser(user);
+    public String saveNewUser(@ModelAttribute("user") User user) {
+        userService.saveUser(user); //сохраняем нового пользователя
         return "redirect:/";
     }
 
 
     @GetMapping("/editInfo/{id}")
-    public String editInfoUser(@PathVariable("id") Long id, Model model) {
+    public String editInfoUser(@PathVariable("id") Long id, Model model) {  //редактируем информацию о пользователе
         model.addAttribute("user", userService.getUserById(id));
         return "editInfoUser";
     }
 
 
     @PatchMapping("/updeteInfo")
-    public String saveEditInfoUser(@ModelAttribute("user") User user) {
+    public String saveEditInfoUser(@ModelAttribute("user") User user) { //сохраняем отредактированную информацию о пользователе
         userService.update(user);
         return "redirect:/";
     }
